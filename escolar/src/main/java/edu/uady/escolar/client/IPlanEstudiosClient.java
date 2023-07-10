c2d75e0298b1534e788cb4a74aafd8931c2de7ea
Wilder Turriza <turriza.poot.wilder@gmail.com>
2023-07-10 05:44:15 -0600

    Gateway

--- escolar/src/main/java/edu/uady/escolar/client/IPlanEstudiosClient.java
+++ escolar/src/main/java/edu/uady/escolar/client/IPlanEstudiosClient.java
@@ -2,13 +2,14 @@ package edu.uady.escolar.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import edu.uady.escolar.dto.client.LicenciaturaMateriaDTO;
 
 @FeignClient(name = "coa-api", path = "/coa-api/plan-estudios", url = "")
-public interface IPlanEstudioClient {
+public interface IPlanEstudiosClient {
 
     @GetMapping(value = ("/{licenciatura-id}"))
     ResponseEntity<LicenciaturaMateriaDTO> findByLicenciaturaId(@PathVariable(value = "licenciatura-id") long licenciatruaId);
